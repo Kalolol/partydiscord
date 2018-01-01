193bffe7b6b888245cfdc78c8cd03c37216835f7
Skye Viau <skye.viau@protonmail.com>
2018-01-01 03:07:08 -0500

    Code clean up, fix #1, and fix #2 with extras

--- index.js
+++ index.js
@@ -8,14 +8,59 @@ const rimraf = require('rimraf');
 const client = new Discord.Client();
 
 const token = config.token;
-var congaparrot = client.emojis.get("394829152005521411");
-var confusedparrot = client.emojis.get("394835129031458826");
-var ultrafastparrot = client.emojis.get("394835185977262080");
-var thumbsupparrot = client.emojis.get("394835153966333952");
 
 function error(message){
   console.log(message)
 }
+
+const helpEmbed = {
+  "title": "About & Commands",
+  "description": "Created by [Kizzaris](http://www.skyenet.ga) to ease upload of the [Cult of the Party Parrot](http://cultofthepartyparrot.com/) emojis.",
+  "color": 2456945,
+  "timestamp": "2018-01-01T06:39:10.973Z",
+  "footer": {
+    "icon_url": "http://cultofthepartyparrot.com/parrots/parrot.gif",
+    "text": "Mr.Parrot (partydiscord)"
+  },
+  "thumbnail": {
+    "url": "http://cultofthepartyparrot.com/parrots/parrot.gif"
+  },
+  "author": {
+    "name": "Mr.Parrot",
+    "url": "https://kizzaris.github.io/partydiscord/",
+    "icon_url": "http://cultofthepartyparrot.com/parrots/parrot.gif"
+  },
+  "fields": [
+    {
+      "name": "Source Code",
+      "value": "You can find it on the [Github](https://github.com/Kizzaris/partydiscord)."
+    },
+    {
+      "name": "Discord Server",
+      "value": "If you need support, join the [Discord Server](https://discord.gg/BG3AUfh)."
+    },
+    {
+      "name": "start",
+      "value": "Starts the upload. (Must have manage emojis).",
+      "inline": true
+    },
+    {
+      "name": "help",
+      "value": "This command right here.",
+      "inline": true
+    },
+    {
+      "name": "leave",
+      "value": "<a:gothparrot:394867688289206272>",
+      "inline": true
+    },
+    {
+      "name": "stats",
+      "value": "Mr.Parrot's statistics.",
+      "inline": true
+    }
+  ]
+};
 
 function unpack() {
   var zip = new AdmZip("./parrot.zip");
@@ -39,72 +84,88 @@ function download() {
 
 client.on('ready', () => {
   console.log('I am ready!');
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setGame('Do +help for help!');
 });
 
-
-client.on('message', message => {
-  if (message.content === 'start') {
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.content === '+start') {
     if (message.channel.type === 'text') {
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> Let's get this started!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> Uploading Emojis!!!!!!!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.guild.createEmoji('./parrots/hd/parrot.gif', 'partyparrot')
-      message.channel.send('partyparrot');
-      message.channel.guild.createEmoji('./parrots/hd/middleparrot.gif', 'middleparrot')
-      message.channel.send('middleparrot');
-      message.channel.guild.createEmoji('./parrots/hd/rightparrot.gif', 'rightyparrot')
-      message.channel.send('rightyparrot');
-      message.channel.guild.createEmoji('./parrots/hd/congaparrot.gif', 'congaparrot')
-      message.channel.send('congaparrot');
-      message.channel.guild.createEmoji('./parrots/hd/shuffleparrot.gif', 'shuffleparrot')
-      message.channel.send('shuffleparrot');
-      message.channel.guild.createEmoji('./parrots/fastparrot.gif', 'fastparrot')
-      message.channel.send('fastparrot');
-      message.channel.guild.createEmoji('./parrots/ultrafastparrot.gif', 'ultrafastparrot')
-      message.channel.send('ultrafastparrot');
-      message.channel.guild.createEmoji('./parrots/christmasparrot.gif', 'christmasparrot')
-      message.channel.send('christmasparrot');
-      message.channel.guild.createEmoji('./parrots/parrotwave1.gif', 'parrotwave1')
-      message.channel.send('parrotwave1');
-      message.channel.guild.createEmoji('./parrots/parrotwave2.gif', 'parrotwave2')
-      message.channel.send('parrotwave2');
-      message.channel.guild.createEmoji('./parrots/parrotwave3.gif', 'parrotwave3')
-      message.channel.send('parrotwave3');
-      message.channel.guild.createEmoji('./parrots/parrotwave4.gif', 'parrotwave4')
-      message.channel.send('parrotwave4');
-      message.channel.guild.createEmoji('./parrots/parrotwave5.gif', 'parrotwave5')
-      message.channel.send('parrotwave5');
-      message.channel.guild.createEmoji('./parrots/parrotwave6.gif', 'parrotwave6')
-      message.channel.send('parrotwave6');
-      message.channel.guild.createEmoji('./parrots/parrotwave7.gif', 'parrotwave7')
-      message.channel.send('parrotwave7');
-      message.channel.guild.createEmoji('./parrots/confusedparrot.gif', 'confusedparrot')
-      message.channel.send('confusedparrot');
-      message.channel.guild.createEmoji('./parrots/dealwithitparrot.gif', 'dealwithitparrot')
-      message.channel.send('dealwithitparrot');
-      message.channel.guild.createEmoji('./parrots/gothparrot.gif', 'gothparrot')
-      message.channel.send('gothparrot');
-      message.channel.guild.createEmoji('./parrots/loveparrot.gif', 'loveparrot')
-      message.channel.send('loveparrot');
-      message.channel.guild.createEmoji('./parrots/parrotcop.gif', 'parrotcop')
-      message.channel.send('parrotcop');
-      message.channel.guild.createEmoji('./parrots/explodyparrot.gif', 'explodyparrot')
-      message.channel.send('explodyparrot');
-      message.channel.guild.createEmoji('./parrots/thumbsupparrot.gif', 'thumbsupparrot')
-      message.channel.send('thumbsupparrot');
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> It's finshed! Enjoy!!!!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
-      message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+      if (message.member.hasPermission("MANAGE_EMOJIS")){
+        uploadEmojis(message);
+      }
     }
-  } else if (message.content === 'clear') {
-    if (message.channel.type === 'dm') {
-      
-    }
+  } else if (message.content === '+help') {
+    message.channel.send({ embed: helpEmbed });
+  } else if (message.content === '+leave') {
+    leaveServer(message);
+  } else if (message.content === '+stats') {
+    message.channel.send(`${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
   }
 });
+
+
+function leaveServer(message) {
+  message.channel.send('<a:gothparrot:394867688289206272>')
+  message.channel.guild.leave()
+}
+
+function uploadEmojis(message) {
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> Let's get this started!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> Uploading Emojis!!!!!!!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.guild.createEmoji('./parrots/hd/parrot.gif', 'partyparrot')
+  message.channel.send('partyparrot');
+  message.channel.guild.createEmoji('./parrots/hd/middleparrot.gif', 'middleparrot')
+  message.channel.send('middleparrot');
+  message.channel.guild.createEmoji('./parrots/hd/rightparrot.gif', 'rightyparrot')
+  message.channel.send('rightyparrot');
+  message.channel.guild.createEmoji('./parrots/hd/congaparrot.gif', 'congaparrot')
+  message.channel.send('congaparrot');
+  message.channel.guild.createEmoji('./parrots/hd/shuffleparrot.gif', 'shuffleparrot')
+  message.channel.send('shuffleparrot');
+  message.channel.guild.createEmoji('./parrots/fastparrot.gif', 'fastparrot')
+  message.channel.send('fastparrot');
+  message.channel.guild.createEmoji('./parrots/ultrafastparrot.gif', 'ultrafastparrot')
+  message.channel.send('ultrafastparrot');
+  message.channel.guild.createEmoji('./parrots/christmasparrot.gif', 'christmasparrot')
+  message.channel.send('christmasparrot');
+  message.channel.guild.createEmoji('./parrots/parrotwave1.gif', 'parrotwave1')
+  message.channel.send('parrotwave1');
+  message.channel.guild.createEmoji('./parrots/parrotwave2.gif', 'parrotwave2')
+  message.channel.send('parrotwave2');
+  message.channel.guild.createEmoji('./parrots/parrotwave3.gif', 'parrotwave3')
+  message.channel.send('parrotwave3');
+  message.channel.guild.createEmoji('./parrots/parrotwave4.gif', 'parrotwave4')
+  message.channel.send('parrotwave4');
+  message.channel.guild.createEmoji('./parrots/parrotwave5.gif', 'parrotwave5')
+  message.channel.send('parrotwave5');
+  message.channel.guild.createEmoji('./parrots/parrotwave6.gif', 'parrotwave6')
+  message.channel.send('parrotwave6');
+  message.channel.guild.createEmoji('./parrots/parrotwave7.gif', 'parrotwave7')
+  message.channel.send('parrotwave7');
+  message.channel.guild.createEmoji('./parrots/confusedparrot.gif', 'confusedparrot')
+  message.channel.send('confusedparrot');
+  message.channel.guild.createEmoji('./parrots/dealwithitparrot.gif', 'dealwithitparrot')
+  message.channel.send('dealwithitparrot');
+  message.channel.guild.createEmoji('./parrots/gothparrot.gif', 'gothparrot')
+  message.channel.send('gothparrot');
+  message.channel.guild.createEmoji('./parrots/loveparrot.gif', 'loveparrot')
+  message.channel.send('loveparrot');
+  message.channel.guild.createEmoji('./parrots/parrotcop.gif', 'parrotcop')
+  message.channel.send('parrotcop');
+  message.channel.guild.createEmoji('./parrots/explodyparrot.gif', 'explodyparrot')
+  message.channel.send('explodyparrot');
+  message.channel.guild.createEmoji('./parrots/thumbsupparrot.gif', 'thumbsupparrot')
+  message.channel.send('thumbsupparrot');
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952> It's finshed! Enjoy!!!!!!! <a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+  message.channel.send(`<a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952><a:ultrafastparrot:394835153966333952>`)
+}
 
 if (fs.existsSync(__dirname + '/parrots/')) {
   rimraf.sync(__dirname + '/parrots/')
